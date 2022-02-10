1dac7c0469336a6ae55cf22f8b3551edbc9342a4
Collin Osborne <89503725+s3vrus@users.noreply.github.com>
2022-02-10 13:02:19 -0500

    media queries and hamburger menu

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,9 @@ import { Link } from "gatsby"
 import '../styles/styles.scss';
 
 const Header = () => {
+
+  const [isOpen, setOpen] = React.useState(false);
+
   return(
     <nav className="navbar">
       <div className="wrapper">
@@ -21,6 +24,22 @@ const Header = () => {
             Contact
           </Link>
         </div>
+
+        <div onClick={() => setOpen(!isOpen)} className={`hamburger-menu ${isOpen ? "open" : "close"}`}>
+          Menu
+          <div className={`panel ${isOpen ? "open" : "close"}`}>
+            <Link to="/about" className="panel-text">
+              About
+            </Link>
+            <Link className="disable panel-text">
+              Work
+            </Link>
+            <Link href = "mailto:hello@collinosborne.dev" className="panel-text">
+              Contact
+            </Link>
+          </div>
+        </div>
+
         </div>
       </div>
     </nav>
