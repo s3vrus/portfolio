52be4ca3339016365ad524527154114b9b5c6bc3
Collin Osborne <89503725+s3vrus@users.noreply.github.com>
2022-02-23 20:25:24 -0500

    quick fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,9 @@
 import * as React from "react"
 
 import Layout from "../components/layout"
-import Seo from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <Seo title="404: Not found" />
     <h1 className="error-page">404: Not Found</h1>
   </Layout>
 )
